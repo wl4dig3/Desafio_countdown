bcaa22b037d3bb0d06b0731d6604f68e53703069
Wladimir Guevara <wladige@gmail.com>
2020-08-30 14:23:24 -0400

    agregando botones de 30 segs, 10 y 5 segs

--- countdown/src/App.vue
+++ countdown/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
     <h2>Countdown</h2>
-    <h1>{{contadorSeg }} seg</h1>
+    <span>{{contadorSeg }} seg</span>
+    <span>{{contadorSeg2 }} seg</span>
+    <span>{{contadorSeg3 }} seg</span>
+    <span>{{contadorSeg4 }} seg</span>
     <div class="botones">
-      <button @click="iniciar(59)">press me</button>
+      <button @click="iniciar1(60)">60 Segs</button>
+      <button @click="iniciar2(30)">30 Segs</button>
+      <button @click="iniciar3(10)">10 Segs</button>
+      <button @click="iniciar4(5)">5 Segs</button>
+
+
+
     </div>
   </div>
 </template>
@@ -14,18 +23,45 @@ export default {
 
   data() {
     return {
-      contadorMin: 0,
-      contadorSeg: 0,
+      // contadorMin: 0,
+      contadorSeg: 60,
+      contadorSeg2:30,
+      contadorSeg3:10,
+      contadorSeg4:5,
       myIntervalo: null,
     };
   },
 
   methods: {
-    iniciar(time) {
+    iniciar1(time) {
       this.contadorSeg = time;
       clearInterval(this.myIntervalo);
       this.myIntervalo = setInterval(() => {
         this.contadorSeg > 0 ? this.contadorSeg-- : false;
+        console.log("Soy el intervalo >:-)");
+      }, 1000);
+    },
+    iniciar2(time) {
+      this.contadorSeg2 = time;
+      clearInterval(this.myIntervalo);
+      this.myIntervalo = setInterval(() => {
+        this.contadorSeg2 > 0 ? this.contadorSeg2-- : false;
+        console.log("Soy el intervalo >:-)");
+      }, 1000);
+    },
+    iniciar3(time) {
+      this.contadorSeg3 = time;
+      clearInterval(this.myIntervalo);
+      this.myIntervalo = setInterval(() => {
+        this.contadorSeg3 > 0 ? this.contadorSeg3-- : false;
+        console.log("Soy el intervalo >:-)");
+      }, 1000);
+    },
+    iniciar4(time) {
+      this.contadorSeg4 = time;
+      clearInterval(this.myIntervalo);
+      this.myIntervalo = setInterval(() => {
+        this.contadorSeg4 > 0 ? this.contadorSeg4-- : false;
         console.log("Soy el intervalo >:-)");
       }, 1000);
     },
@@ -55,5 +91,6 @@ export default {
     border-radius: 0.3rem;
     font-weight: 700;;
   }
+  
 }
 </style>
