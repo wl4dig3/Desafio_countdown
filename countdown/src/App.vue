344f6ab412e3cea6b8a2315badd66975f89d5422
Wladimir Guevara <wladige@gmail.com>
2020-08-30 14:46:00 -0400

    insertando comentarios finales

--- countdown/src/App.vue
+++ countdown/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <h2>Countdown</h2>
+    <!-- se insertan span por cada contador a mostrar -->
     <span>{{contadorSeg }} seg</span>
     <span>{{contadorSeg2 }} seg</span>
     <span>{{contadorSeg3 }} seg</span>
     <span>{{contadorSeg4 }} seg</span>
+    <!-- se sgrupan en una div los botones  -->
     <div class="botones">
       <button @click="iniciar1(60)">60 Segs</button>
       <button @click="iniciar2(30)">30 Segs</button>
       <button @click="iniciar3(10)">10 Segs</button>
       <button @click="iniciar4(5)">5 Segs</button>
-
-
-
     </div>
   </div>
 </template>
@@ -23,15 +22,15 @@ export default {
 
   data() {
     return {
-      // contadorMin: 0,
+      // return por cada contador,
       contadorSeg: 60,
-      contadorSeg2:30,
-      contadorSeg3:10,
-      contadorSeg4:5,
+      contadorSeg2: 30,
+      contadorSeg3: 10,
+      contadorSeg4: 5,
       myIntervalo: null,
     };
   },
-
+    // se crean metodos por cada boton 
   methods: {
     iniciar1(time) {
       this.contadorSeg = time;
@@ -68,7 +67,7 @@ export default {
   },
 };
 </script>
-
+      // estilos css personales 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -77,20 +76,26 @@ export default {
   text-align: center;
   color: #83add8;
   margin-top: 60px;
-  background-image:url(assets/bg.jpg);
+  background-image: url(assets/bg.jpg);
   width: 60%;
+  height: 15rem;
   margin: 10rem auto;
   padding: 3rem 0;
+  border-radius: 0.4rem;
 
   h2 {
     color: blue;
     font-size: 2em;
   }
   button {
+    margin-top: 0.6rem;
+    margin-right: 0.1rem;
     padding: 0.4rem 0.9rem;
     border-radius: 0.3rem;
-    font-weight: 700;;
+    font-weight: 700;
   }
-  
+  span {
+    margin-right: 1.7rem;
+  }
 }
 </style>
